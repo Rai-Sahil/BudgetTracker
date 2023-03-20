124db6b1b670dddd35088ee5e32fc326c227dd7a
Traill Lyall <holycrapeh@hotmail.com>
2023-03-20 13:09:01 -0700

    main sidebar done

--- app/src/main/java/com/bcit/budgetapp/MainActivity.kt
+++ app/src/main/java/com/bcit/budgetapp/MainActivity.kt
@@ -2,12 +2,42 @@ package com.bcit.budgetapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity()
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener
 {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView_main) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        val navigationView = findViewById<NavigationView>(R.id.navigationView_main)
+        navigationView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean
+    {
+        item.onNavDestinationSelected(navController)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean
+    {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
